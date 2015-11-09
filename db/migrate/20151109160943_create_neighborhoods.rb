165be10d8daa84db730b5fe05e2ba33004f0e10a
sarabrent <sarabrentmccoy@gmail.com>
2015-11-09 11:12:48 -0500

    Adds to CreateNeighborhoods migration file these params:
    	name
    	city
    	greater_metro_area

--- db/migrate/20151109160943_create_neighborhoods.rb
+++ db/migrate/20151109160943_create_neighborhoods.rb
@@ -1,6 +1,9 @@
 class CreateNeighborhoods < ActiveRecord::Migration
   def change
     create_table :neighborhoods do |t|
+    	t.string :name
+    	t.string :city
+    	t.string :greater_metro_area
     end
   end
 end
