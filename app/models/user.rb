4e376585fd23cedf3e923dc14c7b7919f0e562f8
Lauren Grant <lgrant1286@gmail.com>
2015-11-09 14:58:54 -0500

    Updates model for user.
    Do not need in model authentication. Authentication.rb file has that information already.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
   class User < ActiveRecord::Base
+    include Authentication
+
     has_many :events
     has_many :locations, through: :events
+
+    # validates :password, presence: true
+    # validates :email, uniqueness: true
   end
