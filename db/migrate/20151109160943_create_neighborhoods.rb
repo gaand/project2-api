422693b888aead3131bc8fc4f114a64c4e61ca85
sarabrent <sarabrentmccoy@gmail.com>
2015-11-10 15:14:06 -0500

    Deletes neighborhoods migration file.

--- db/migrate/20151109160943_create_neighborhoods.rb
+++
@@ -1,9 +0,0 @@
-class CreateNeighborhoods < ActiveRecord::Migration
-  def change
-    create_table :neighborhoods do |t|
-    	t.string :name
-    	t.string :city
-    	t.string :greater_metro_area
-    end
-  end
-end
