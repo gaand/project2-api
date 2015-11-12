f4a872de446e54ddaba5772aa00f35473ddc3cfd
Lauren Grant <lgrant1286@gmail.com>
2015-11-12 16:27:20 -0500

    Adjusts belongs_to relationship with User and Location.
    One event can only belong to one of each.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
   class Event < ActiveRecord::Base
-    belongs_to :user, inverse_of: :locations
-    belongs_to :location, inverse_of: :users
+    belongs_to :user
+    belongs_to :location
+
+    # belongs_to :user, inverse_of: :locations
+    # belongs_to :location, inverse_of: :users
+
   end
