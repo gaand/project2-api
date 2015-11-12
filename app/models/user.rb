f918fd223164a741dced5a36c90fdab450a85601
Lauren Grant <lgrant1286@gmail.com>
2015-11-12 16:29:10 -0500

    Deletes has_many relationship with location.
    The events have the location, not the user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
     include Authentication
 
     has_many :events
-    has_many :locations, through: :events
+    # has_many :locations, through: :events
 
     # validates :password, presence: true
     # validates :email, uniqueness: true
