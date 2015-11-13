f00fe74f22d46ca2dd8ee480cdc15e64fd56754c
Lauren Grant <lgrant1286@gmail.com>
2015-11-13 00:26:05 -0500

    Finalizes working routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,16 @@ Rails.application.routes.draw do
   post '/login' => 'auth#login'
   delete '/logout/:id' => 'auth#logout'
 
+  get '/events', to: 'events#index'
+  post '/events/:id', to: 'events#create'
+  patch '/events/:id', to: 'events#update'
+
   resources :users, except: [:new, :edit]
 
   resources :events, except: [:new, :edit]
-end
+
+    # create routes for event resource
+  # resources :events, except: [:new, :edit] do
+    # create nested routes for the events locations
+  # resources :locations, except: [:new, :edit, :update]
+  end
