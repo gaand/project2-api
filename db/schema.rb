7225e6a15ef73cc469cb959e0d1885e8f469505b
Antony Donovan <antony.donovan@generalassemb.ly>
2016-11-13 22:26:15 -0500

    Use non-date prefixes for template provided migrations
    These migrations will be updated when necessary.
    Don't imply stability from date prefix forward.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327214720) do
+ActiveRecord::Schema.define(version: 200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
